10236ae36c741da31239b539ea453c1230f64f56
Aoyagi Gakuto <k024g0082@g.neec.ac.jp>
2026-06-20 15:18:54 +0900

    これで

--- project/engin/game/cpp/WinApp.cpp
+++ project/engin/game/cpp/WinApp.cpp
@@ -75,7 +75,7 @@ void WinApp::Initialize()
 
     hwnd = CreateWindowW(
         wc.lpszClassName, // 利用するクラス名
-        L"myGame", // タイトルバーの文字
+        L"LE3C_01_アオヤギ_ガクト", // タイトルバーの文字
         WS_OVERLAPPEDWINDOW, // 通常ウィンドウ
         posX, // 表示x座標（画面中央）
         posY, // 表示y座標（画面中央）
